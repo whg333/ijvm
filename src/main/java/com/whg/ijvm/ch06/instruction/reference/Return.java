86136cb55b47e624325dd39bcf09b40e0cc1b822
luojingxin <luojingxin@hulai.com>
2022-03-01 16:18:04 +0800

    ch06 fixed bug

---
+++ src/main/java/com/whg/ijvm/ch06/instruction/reference/Return.java
@@ -0,0 +1,15 @@
+package com.whg.ijvm.ch06.instruction.reference;
+
+import com.whg.ijvm.ch06.instruction.base.NoOperandsInstruction;
+import com.whg.ijvm.ch06.runtime.RFrame;
+
+public class Return {
+
+    public static class RETURN extends NoOperandsInstruction{
+        @Override
+        public void execute(RFrame frame) {
+            // TODO
+        }
+    }
+
+}
